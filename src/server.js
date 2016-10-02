b32ba571d131319db3474fdf3ad6586570a07de4
Sämir Hamadi <samir.hamadi@elisa.fi>
2016-10-02 22:54:50 +0300

    Cors

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const app = express();
+const cors = require('cors');
 const rootHandler = require('./handlers/rootHandler');
 const errorHandler = require('./handlers/errorHandler')
 
 app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/', rootHandler);
