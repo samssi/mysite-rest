482b9294cc440e23635bc66efeb5d75dfa6edd66
Sämir Hamadi <samir.hamadi@gmail.com>
2017-01-03 22:08:27 +0200

    Refactor to handler

--- src/handlers/rootHandler.js
+++ src/handlers/rootHandler.js
@@ -3,6 +3,40 @@
 const express = require('express');
 const contentHandle = require('../handles/contentHandle');
 const router = express.Router();
+const app = express();
+
+function requireToken(req, res, next) {
+    console.log('route protection!');
+    const token = req.get('Authorization');
+    console.log('Request headers: ' + token);
+
+    if (token) {
+        console.log('token found: ' + token);
+        returnTokenOrFail(token, next, req, res);
+    }
+    else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+}
+
+function returnTokenOrFail(token, next, req, res) {
+    jwt.verify(token, secret, function(err, decodedToken) {
+        if(!err) {
+            req.decodedToken = decodedToken;
+            next();
+        }
+        else {
+            return res.json({ success: false, message: 'Failed to authenticate token.' });
+        }
+    });
+}
+
+app.all("/contents/*", requireToken, function(req, res, next) {
+    next();
+});
 
 router.get('/contents/personals', contentHandle.personalInfosHandle);
 router.get('/contents/experiences', contentHandle.experiencesHandle);
--- src/server.js
+++ src/server.js
@@ -16,39 +16,6 @@ const secret = 'very secret';
 
 app.use(cors());
 
-function requireToken(req, res, next) {
-    console.log('route protection!');
-    const token = req.get('Authorization');
-    console.log('Request headers: ' + token);
-
-    if (token) {
-        console.log('token found: ' + token);
-        returnTokenOrFail(token, next, req, res);
-    }
-    else {
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-    }
-}
-
-function returnTokenOrFail(token, next, req, res) {
-    jwt.verify(token, secret, function(err, decodedToken) {
-        if(!err) {
-            req.decodedToken = decodedToken;
-            next();
-        }
-        else {
-            return res.json({ success: false, message: 'Failed to authenticate token.' });
-        }
-    });
-}
-
-app.all("/contents/*", requireToken, function(req, res, next) {
-    next();
-});
-
 app.use(router);
 
 app.use(helmet());
