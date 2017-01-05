18ca8ca9efad70a16eaa674399dbd1a61f053bcd
Sämir Hamadi <samir.hamadi@gmail.com>
2017-01-06 00:47:34 +0200

    DRY

--- src/service/mongoDbService.js
+++ src/service/mongoDbService.js
@@ -3,29 +3,27 @@
 const bunyan = require('bunyan');
 const logger = bunyan.createLogger({name: 'mysite-rest-mongoDbService'});
 
-function getFirst(db, collection, res) {
-    db.collection(collection).findOne(function(err, results) {
-        if (err) {
-            logger.error(err, "Error finding collection: " + collection);
-            res.sendStatus(500);
-        }
-        res.status(200).send(results);
-    });
-}
-function getAll(db, collection, res) {
-    db.collection(collection).find().toArray(function(err, results) {
-        if (err) {
-            logger.error(err, "Error finding collection: " + collection);
-            res.sendStatus(500);
-        }
-        res.status(200).send(results);
-    });
-}
-
 function executeDbRequest(req, res, document, func) {
     logger.info("User '%s' requested for %s", req.app.locals.decodedToken.sub, document);
     const db = req.app.locals.db;
     func(db, document, res);
+}
+
+
+function getFirst(db, collection, res) {
+    db.collection(collection).findOne(function(err, results) {returnResults(err, results, res)});
+}
+
+function getAll(db, collection, res) {
+    db.collection(collection).find().toArray(function(err, results) {returnResults(err, results, res)});
+}
+
+function returnResults(err, results, res) {
+    if (err) {
+        logger.error(err, "Error finding collection: " + collection);
+        res.sendStatus(500);
+    }
+    res.status(200).send(results);
 }
 
 module.exports = {getAll, getFirst, executeDbRequest };
