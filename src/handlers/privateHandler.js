6b2935139bdd1667fd87c798983375aabacb6c78
Sämir Hamadi <samir.hamadi@gmail.com>
2017-02-11 21:58:05 +0200

    Refactor urls

--- src/handlers/privateHandler.js
+++ src/handlers/privateHandler.js
@@ -8,10 +8,10 @@ const mongoDbService = require('../service/mongoDbService');
 
 router.all("/*", routeProtection.requireToken);
 
-router.get('/contents/applications', function(req,res) {mongoDbService.executeDbRequest(req, res, "application", mongoDbService.getFirst)});
-router.get('/contents/personals', function(req,res) {mongoDbService.executeDbRequest(req, res, "personalInfo", mongoDbService.getFirst)});
-router.get('/contents/experiences', function(req,res) {mongoDbService.executeDbRequest(req, res, "experience", mongoDbService.getAll)});
-router.get('/contents/portfolios', function(req,res) {mongoDbService.executeDbRequest(req, res, "portfolio", mongoDbService.getAll)});
+router.get('/contents/application', function(req,res) {mongoDbService.executeDbRequest(req, res, "application", mongoDbService.getFirst)});
+router.get('/contents/personal', function(req,res) {mongoDbService.executeDbRequest(req, res, "personalInfo", mongoDbService.getFirst)});
+router.get('/contents/experience', function(req,res) {mongoDbService.executeDbRequest(req, res, "experience", mongoDbService.getAll)});
+router.get('/contents/portfolio', function(req,res) {mongoDbService.executeDbRequest(req, res, "portfolio", mongoDbService.getAll)});
 router.get('/contents/about', function(req,res) {mongoDbService.executeDbRequest(req, res, "about", mongoDbService.getFirst)});
 
 module.exports = router;
